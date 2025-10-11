7544badf381bd73e51d9ab66a468abfeb4f2f0ab
samitha278 <samithasahanssb@gmail.com>
2025-10-11 16:56:30 +0530

    custom Dataloader for imagenet but inefficient

--- src/train_imagenet.py
+++ src/train_imagenet.py
@@ -2,14 +2,17 @@ import torch
 import torch.nn as nn
 from torchvision import transforms
 from datasets import load_dataset
+import random
 
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-# Data pre
+# Data pre ----------------------------------------------------------------------------
 
+batch_size = 32
 
-# Load dataset (dataset object : "train" : 126689 , "val" : 5000 splits , 100 classes) 
+# Load dataset (dataset object : "train" : 126689 images , "val" : 5000 imagess , 100 classes) 
 dataset = load_dataset("/home/samitha/Projects/datasets/imagenet100")
 
 
@@ -18,7 +21,7 @@ train_transform = transforms.Compose([
     transforms.Resize(256),
     transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
-    transforms.AutoAugment(num_ops=2, magnitude=9),   # ViT style
+    transforms.RandAugment(num_ops=2, magnitude=9),   # ViT style
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
@@ -33,36 +36,46 @@ val_transform = transforms.Compose([
 
 
 
-class DataLoader(nn.Module):
-    def __init__(self,data,transform,batch_size):
-        super().__init__()
+class DataLoader():
+    def __init__(self,data,transform,batch_size,Shuffle= False):
         
-        self.data = data
         self.transform = transform
         self.batch_size = batch_size
         
+        self.data = list(zip(data['image'],data['label']))  # make (image,label) tuples list
+        if Shuffle:
+            random.shuffle(self.data)    # in place random shuffle of data 
+            
         self.count = 0
         
-    def forward(self):
+        
+    def get_batch(self):
         
         batch = self.data[self.count : self.count + self.batch_size]  # taking batch
+        imgs,labels = zip(*batch)    # (img1,lb1),(img2,lb2) -> (im1,img2) , (lb2,lb2)
+        
+        xb = torch.stack([self.transform(img) for img in imgs])
+        yb = torch.tensor(labels)
+        
         self.count += self.batch_size   # update for next batch
-        
-        
-        xb = [self.transform(img) for img in batch['image']]
-        yb = batch['label']
-        
-        
-        
-        
+        if self.count > len(self.data): # Reset for next epoch
+            self.count = 0 
+            random.shuffle(self.samples)
+            
+        return xb,yb 
         
         
         
-        
-    
+# Initialize Data train,val
+train_loader = DataLoader(dataset['train'],train_transform,batch_size,Shuffle=True)
+val_loader = DataLoader(dataset['validation'],val_transform,batch_size)
+
+
+# temp get data
+xb,yb = train_loader.get_batch()
+xb,yb = xb.to(device),yb.to(device)
+print(xb,yb)
 
 
 
-
-
-# Train 
+# Train ----------------------------------------------------------------------------
