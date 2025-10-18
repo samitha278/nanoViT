58119befb801204f6d7802f5af5d765a36662a24
samitha278 <samithasahanssb@gmail.com>
2025-10-18 11:34:50 +0530

    fiexd bugs

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -5,7 +5,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.models.vit import ViT, ViTBaseConfig
-from src.evaluation import evaluate
+from src.evaluation.evaluation import evaluate
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -19,14 +19,16 @@ config = ViTBaseConfig()
 model = ViT(config)
 
 checkpoint = torch.load('/home/samitha/Projects/nanoViT/src/log/swa_model_29to_last.pt',map_location='cuda') 
-model.load_state_dict(checkpoint['model'])
+
+
+model.load_state_dict(checkpoint)
 model.to(device)
 
 # Load val data
 from src.data.dataset import get_val_dataloader
-val_loader = get_val_dataloader("/home/samitha/Projects/datasets/imagenet100")
+val_loader,class_names = get_val_dataloader("/home/samitha/Projects/datasets/imagenet100",classes=True)
 
 model.eval()
-results = evaluate(model, val_loader, device)
+results = evaluate(model, val_loader, class_names,device)
 
 print(results)
